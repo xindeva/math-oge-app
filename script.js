f92f17c43f9bbae582db5bcf5d5fcbcf7df4bfc6
xindeva <127549644+xindeva@users.noreply.github.com>
2025-08-20 19:14:49 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Данные уровней (вставь свои вопросы и фото-ссылки)
+const levels = [
+    {
+        name: 'Уровень 1 (10 вопросов)',
+        questions: [
+            { text: 'Решите уравнение: x² - 5x + 6 = 0', options: ['x=2, x=3', 'x=1, x=6', 'x=0, x=5', 'x=-2, x=-3'], correct: 0, photo: 'https://example.com/photo1.jpg' },
+            //
